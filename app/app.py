3fc813d3ae3fe9d7ab36095dc0ff007bdeae8c7a
Vidyesh Churi <vidyesh95@gmail.com>
2025-11-24 10:18:09 +0530

    feat: add multiple sample text posts to `text_posts` dictionary.

--- app/app.py
+++ app/app.py
@@ -2,7 +2,18 @@ from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI()
 
-text_posts = {1: {"title":"New Post", "content":  "cool test post"}}
+text_posts = {
+    1: {"title":"New Post", "content":  "Lorem ipsum Bug Catcher traveled from Goldenrod City to Ilex Forest via Route 34.Lorem ipsum You can find the Azalea Gym in Johto."},
+    2: {"title":"Old Post", "content":  "Lorem ipsum Kindler journeyed along Route 9 between Route 10 and Cerulean City."},
+    3: {"title":"New Post", "content":  "Lorem ipsum Bug Catcher traveled from Goldenrod City to Ilex Forest via Route 34."},
+    4: {"title":"New Post", "content":  "Lorem ipsum Team Plasma's favorite Pokémon is Snorlax."},
+    5: {"title":"Old Post", "content":  "Lorem ipsum You can earn the Soul Badge at Fuchsia Gym."},
+    6: {"title":"New Post", "content":  "Lorem ipsum Hoopster spotted Butterfree in Kalos."},
+    7: {"title":"New Post", "content":  "Lorem ipsum Metapod evolved into Butterfree."},
+    8: {"title":"New Post", "content":  "Lorem ipsum Omanyte evolved into Omastar."},
+    9: {"title":"New Post", "content":  "Lorem ipsum You can earn the Fighting Badge at Stow-on-Side Stadium."},
+    10: {"title":"New Post", "content":  "Lorem ipsum Machoke used Seismic Toss."},
+}
 
 @app.get("/posts")
 def get_all_posts():
