ecb08d3d55d80c68f61c2be5bafde32b709bac9d
Vidyesh Churi <vidyesh95@gmail.com>
2025-11-24 11:18:42 +0530

    feat: Add POST /posts endpoint for creating new posts and reformat existing dictionary and HTTPException calls.

--- app/app.py
+++ app/app.py
@@ -1,19 +1,41 @@
-from fastapi import FastAPI,HTTPException, status
+from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI()
 
 text_posts = {
-    1: {"title":"New Post", "content":  "Lorem ipsum Bug Catcher traveled from Goldenrod City to Ilex Forest via Route 34.Lorem ipsum You can find the Azalea Gym in Johto."},
-    2: {"title":"Old Post", "content":  "Lorem ipsum Kindler journeyed along Route 9 between Route 10 and Cerulean City."},
-    3: {"title":"New Post", "content":  "Lorem ipsum Bug Catcher traveled from Goldenrod City to Ilex Forest via Route 34."},
-    4: {"title":"New Post", "content":  "Lorem ipsum Team Plasma's favorite Pokémon is Snorlax."},
-    5: {"title":"Old Post", "content":  "Lorem ipsum You can earn the Soul Badge at Fuchsia Gym."},
-    6: {"title":"New Post", "content":  "Lorem ipsum Hoopster spotted Butterfree in Kalos."},
-    7: {"title":"New Post", "content":  "Lorem ipsum Metapod evolved into Butterfree."},
-    8: {"title":"New Post", "content":  "Lorem ipsum Omanyte evolved into Omastar."},
-    9: {"title":"New Post", "content":  "Lorem ipsum You can earn the Fighting Badge at Stow-on-Side Stadium."},
-    10: {"title":"New Post", "content":  "Lorem ipsum Machoke used Seismic Toss."},
+    1: {
+        "title": "New Post",
+        "content": "Lorem ipsum Bug Catcher traveled from Goldenrod City to Ilex Forest via Route 34.Lorem ipsum You can find the Azalea Gym in Johto.",
+    },
+    2: {
+        "title": "Old Post",
+        "content": "Lorem ipsum Kindler journeyed along Route 9 between Route 10 and Cerulean City.",
+    },
+    3: {
+        "title": "New Post",
+        "content": "Lorem ipsum Bug Catcher traveled from Goldenrod City to Ilex Forest via Route 34.",
+    },
+    4: {
+        "title": "New Post",
+        "content": "Lorem ipsum Team Plasma's favorite Pokémon is Snorlax.",
+    },
+    5: {
+        "title": "Old Post",
+        "content": "Lorem ipsum You can earn the Soul Badge at Fuchsia Gym.",
+    },
+    6: {
+        "title": "New Post",
+        "content": "Lorem ipsum Hoopster spotted Butterfree in Kalos.",
+    },
+    7: {"title": "New Post", "content": "Lorem ipsum Metapod evolved into Butterfree."},
+    8: {"title": "New Post", "content": "Lorem ipsum Omanyte evolved into Omastar."},
+    9: {
+        "title": "New Post",
+        "content": "Lorem ipsum You can earn the Fighting Badge at Stow-on-Side Stadium.",
+    },
+    10: {"title": "New Post", "content": "Lorem ipsum Machoke used Seismic Toss."},
 }
+
 
 @app.get("/posts")
 async def get_all_posts(limit: int | None = None):
@@ -21,9 +43,22 @@ async def get_all_posts(limit: int | None = None):
         return list(text_posts.values())[:limit]
     return text_posts
 
+
 @app.get("/posts/{post_id}")
 async def get_post(post_id: int):
     if post_id not in text_posts:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id {post_id} not found",
+        )
     return text_posts.get(post_id)
 
+
+@app.post("/posts")
+async def create_post(post_id: int):
+    if post_id in text_posts:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Post with id {post_id} already exists",
+        )
+    return text_posts[post_id]
