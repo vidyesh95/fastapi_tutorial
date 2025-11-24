0f3ba48c36c531498a91facb31c780f28f293475
Vidyesh Churi <vidyesh95@gmail.com>
2025-11-24 09:46:18 +0530

    feat: Add Pyrefly configuration and implement an endpoint to retrieve posts by ID.

--- app/app.py
+++ app/app.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-text_posts = {"1": {"title":"New Post", "content":  "cool test post"}}
+text_posts = {1: {"title":"New Post", "content":  "cool test post"}}
 
 @app.get("/posts")
 def get_all_posts():
     return text_posts
+
+@app.get("/posts/{post_id}")
+def get_post(post_id: int):
+    return text_posts.get(post_id)
+
