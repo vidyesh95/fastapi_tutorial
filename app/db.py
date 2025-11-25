911e8a01df55f68da29aff9e545eb95f1e85ab69
Vidyesh Churi <vidyesh95@gmail.com>
2025-11-25 09:46:29 +0530

    feat: Add Post SQLModel with UUID, title, and content fields.

--- app/db.py
+++ app/db.py
@@ -1,7 +1,15 @@
+from uuid import UUID, uuid7
 from typing import Annotated
 from fastapi import Depends
 
-from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel import Field, Session, SQLModel, create_engine
+
+
+class Post(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid7, primary_key=True)
+    title: str | None = Field(default=None, index=True)
+    content: str | None = Field(default=None, index=True)
+
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -9,11 +17,14 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
 
 def get_session():
     with Session(engine) as session:
         yield session
+
 
 SessionDep = Annotated[Session, Depends(get_session)]
