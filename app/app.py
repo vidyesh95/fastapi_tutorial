673a656d2075d70ae575e3570311a9064129cfaa
Vidyesh Churi <vidyesh95@gmail.com>
2025-11-25 14:45:52 +0530

    feat: Replace in-memory post storage with a database-backed `/upload` endpoint and update imports for database interaction.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, HTTPException, status
-from app.schemas import PostCreate, PostResponse
-from app.db import create_db_and_tables
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, status
+# from app.schemas import PostCreate, PostResponse
+from app.db import create_db_and_tables, Post, SessionDep
 from contextlib import asynccontextmanager
 
 
@@ -13,63 +13,63 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-text_posts = {
-    1: {
-        "title": "New Post",
-        "content": "Lorem ipsum Bug Catcher traveled from Goldenrod City to Ilex Forest via Route 34.Lorem ipsum You can find the Azalea Gym in Johto.",
-    },
-    2: {
-        "title": "Old Post",
-        "content": "Lorem ipsum Kindler journeyed along Route 9 between Route 10 and Cerulean City.",
-    },
-    3: {
-        "title": "New Post",
-        "content": "Lorem ipsum Bug Catcher traveled from Goldenrod City to Ilex Forest via Route 34.",
-    },
-    4: {
-        "title": "New Post",
-        "content": "Lorem ipsum Team Plasma's favorite Pokémon is Snorlax.",
-    },
-    5: {
-        "title": "Old Post",
-        "content": "Lorem ipsum You can earn the Soul Badge at Fuchsia Gym.",
-    },
-    6: {
-        "title": "New Post",
-        "content": "Lorem ipsum Hoopster spotted Butterfree in Kalos.",
-    },
-    7: {"title": "New Post", "content": "Lorem ipsum Metapod evolved into Butterfree."},
-    8: {"title": "New Post", "content": "Lorem ipsum Omanyte evolved into Omastar."},
-    9: {
-        "title": "New Post",
-        "content": "Lorem ipsum You can earn the Fighting Badge at Stow-on-Side Stadium.",
-    },
-    10: {"title": "New Post", "content": "Lorem ipsum Machoke used Seismic Toss."},
-}
+# text_posts = {
+#     1: {
+#         "title": "New Post",
+#         "content": "Lorem ipsum Bug Catcher traveled from Goldenrod City to Ilex Forest via Route 34.Lorem ipsum You can find the Azalea Gym in Johto.",
+#     },
+#     2: {
+#         "title": "Old Post",
+#         "content": "Lorem ipsum Kindler journeyed along Route 9 between Route 10 and Cerulean City.",
+#     },
+#     3: {
+#         "title": "New Post",
+#         "content": "Lorem ipsum Bug Catcher traveled from Goldenrod City to Ilex Forest via Route 34.",
+#     },
+#     4: {
+#         "title": "New Post",
+#         "content": "Lorem ipsum Team Plasma's favorite Pokémon is Snorlax.",
+#     },
+#     5: {
+#         "title": "Old Post",
+#         "content": "Lorem ipsum You can earn the Soul Badge at Fuchsia Gym.",
+#     },
+#     6: {
+#         "title": "New Post",
+#         "content": "Lorem ipsum Hoopster spotted Butterfree in Kalos.",
+#     },
+#     7: {"title": "New Post", "content": "Lorem ipsum Metapod evolved into Butterfree."},
+#     8: {"title": "New Post", "content": "Lorem ipsum Omanyte evolved into Omastar."},
+#     9: {
+#         "title": "New Post",
+#         "content": "Lorem ipsum You can earn the Fighting Badge at Stow-on-Side Stadium.",
+#     },
+#     10: {"title": "New Post", "content": "Lorem ipsum Machoke used Seismic Toss."},
+# }
 
 
-@app.get("/posts")
-async def get_all_posts(limit: int | None = None):
-    if limit:
-        return list(text_posts.values())[:limit]
-    return text_posts
+# @app.get("/posts")
+# async def get_all_posts(limit: int | None = None):
+#     if limit:
+#         return list(text_posts.values())[:limit]
+#     return text_posts
 
 
-@app.get("/posts/{post_id}")
-async def get_post(post_id: int):
-    if post_id not in text_posts:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id {post_id} not found",
-        )
-    return text_posts.get(post_id)
+# @app.get("/posts/{post_id}")
+# async def get_post(post_id: int):
+#     if post_id not in text_posts:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND,
+#             detail=f"Post with id {post_id} not found",
+#         )
+#     return text_posts.get(post_id)
 
 
-@app.post("/posts")
-async def create_post(post: PostCreate) -> PostResponse:
-    new_post = {"title": post.title, "content": post.content}
-    text_posts[max(text_posts.keys()) + 1] = new_post
-    return PostResponse(**new_post)
+# @app.post("/posts")
+# async def create_post(post: PostCreate) -> PostResponse:
+#     new_post = {"title": post.title, "content": post.content}
+#     text_posts[max(text_posts.keys()) + 1] = new_post
+#     return PostResponse(**new_post)
 
 # @app.post("/posts")
 # async def create_post(post_id: int, post: PostCreate) -> PostResponse:
@@ -81,3 +81,10 @@ async def create_post(post: PostCreate) -> PostResponse:
 #     new_post = {"title": post.title, "content": post.content}
 #     text_posts[post_id] = new_post
 #     return PostResponse(**new_post)
+
+@app.post("/upload")
+def upload_file(post: Post, session: SessionDep) -> Post:
+    session.add(post)
+    session.commit()
+    session.refresh(post)
+    return post
